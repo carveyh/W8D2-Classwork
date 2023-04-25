fca14714ab917325849e47fc23a0d3f0b8586cf9
dtannyc1 <dtannyc1@gmail.com>
2023-04-25 14:21:39 -0400

    Finished phase 1

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -15,3 +15,42 @@ Array.prototype.uniq = function() {
 console.log([1,2,2,3,3,3].uniq()) // => [1,2,3]
 
 console.log([1,2].__proto__)
+
+//------------------------------------------------------------------------------
+Array.prototype.twoSum = function() {
+    let pairs = [];
+    let positions = ({});
+    for (i = 0; i < this.length; i++) {
+        // debugger
+        opposite = 0-this[i];
+        // if (positions[opposite]) {    // 0 IS FALSEY
+        // if (positions[opposite] !== NaN) {    // it is undefined
+        // will assign to positions negative numbers as string
+        // it can detect opposite numbers if represented as a string in hash
+        if (positions[opposite] !== undefined) {    // 0 IS FALSEY
+            pairs.push([positions[opposite], i]);
+            positions[this[i]] = i;
+        } else {
+            positions[this[i]] = i;
+        }
+    }
+    return pairs;
+}
+
+console.log([1, 0, 7, -1, 8, -7, 6, -7, 1].twoSum())
+
+//------------------------------------------------------------------------------
+Array.prototype.transpose = function() {
+    let transposed = [];
+    for (j = 0; j < this[0].length; j++) {
+        let row = [];
+        for (i = 0; i < this.length; i++) {
+            row.push(this[i][j]);
+        }
+        transposed.push(row);
+    }
+    return transposed;
+}
+console.log("Transpose: ")
+console.log([[1,2,3],[4,5,6]])
+console.log([[1,2,3],[4,5,6]].transpose())
