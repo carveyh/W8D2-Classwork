5dac6ddb6c839836d4e7c71a39a099f3d3e89573
dtannyc1 <dtannyc1@gmail.com>
2023-04-25 17:01:00 -0400

    working on #deepDup

--- skeleton/phase_4_recursion.js
+++ skeleton/phase_4_recursion.js
@@ -2,7 +2,7 @@ const range = function(start, end) {
 	if (start > end) {
 		return [];
 	}
-	
+
 	if (start === end) {
 		return [start];
 	} else {
@@ -35,3 +35,34 @@ const exponent = function(base, exp) {
 
 // ----------------------------------------
 
+const fibonacci = function(n){
+    if (n <= 0) {
+        return [];
+    } else if (n === 1) {
+        return [1];
+    } else if (n === 2) {
+        return [1, 1];
+    } else {
+        let previous = fibonacci(n-1);
+        previous.push(previous[previous.length-1] + previous[previous.length-2]);
+        return previous;
+    }
+};
+
+//------------------------------------------
+const deepDup = function(arr) {
+    copy = [];
+    arr.forEach(el => {
+        if (el instanceof Array) {
+            // arr.forEach(el => {
+            //     copy.push(deepDup(el))
+            // })
+            copy.push(deepDup(el))
+        } else {
+            // something to duplicate base case
+            // return arr;
+            copy.push(el);
+        }
+    });
+    return copy;
+};
